7b4060da6eadf17f346c8b7a288aa08118eff635
Viacheslav Khudiakov <v.khudiakov@netconomy.net>
2015-09-13 13:37:23 +0200

    Bookmarks project setup
    Initial maven data for the project

---
+++ src/main/java/bookmarks/BookmarksApplication.java
@@ -0,0 +1,12 @@
+package bookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarksApplication.class, args);
+    }
+}
---
+++ src/test/java/bookmarks/BookmarksApplicationTests.java
@@ -0,0 +1,18 @@
+package bookmarks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BookmarksApplication.class)
+@WebAppConfiguration
+public class BookmarksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
